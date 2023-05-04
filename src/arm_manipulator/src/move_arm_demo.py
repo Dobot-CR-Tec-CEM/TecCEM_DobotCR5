572dbb1ded32df5a677ec9825855ee2be1f29cbc
Daniela <daniela.aluna@outlook.com>
2023-05-04 16:57:00 -0600

    Arm goes towards goal

--- src/arm_manipulator/src/move_arm_demo.py
+++ src/arm_manipulator/src/move_arm_demo.py
@@ -11,13 +11,23 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 from moveit_commander.conversions import pose_to_list
-from geometry_msgs.msg import Pose, PoseStamped, Quaternion
+from geometry_msgs.msg import Pose, PoseStamped
+
+
+"""
+planning frame : dummy_link
+end efector link: Link6
+planning_group: 'cr5_arm'
+"""
+
 
 
 moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node("cr5_manipulator", anonymous=True)
+rospy.init_node("move_group_python", anonymous=True)
+
 
 robot = moveit_commander.RobotCommander()
+
 scene = moveit_commander.PlanningSceneInterface()
 
 group_name = "cr5_arm"
@@ -61,55 +71,36 @@ def wait_for_state_update(box_name, box_is_known=False, box_is_attached=False, t
       seconds = rospy.get_time()
     return False
 
+# We can get the joint values from the group and adjust some of the values:
+joint_goal = move_group.get_current_joint_values()
+joint_goal[0] = 0
+joint_goal[1] = -np.pi/4
+joint_goal[2] = 0
+joint_goal[3] = -np.pi/2
+joint_goal[4] = 0
+joint_goal[5] = np.pi/3
 
-def go_home_pose():
-    # We can get the joint values from the group and adjust some of the values:
-    joint_goal = move_group.get_current_joint_values()
-    joint_goal[0] = 0
-    joint_goal[1] = 0
-    joint_goal[2] = 0
-    joint_goal[3] = 0
-    joint_goal[4] = 0
-    joint_goal[5] = 0
-    # The go command can be called with joint values, poses, or without any
-    # parameters if you have already set the pose or joint target for the group
-    move_group.go(joint_goal, wait=True)
-    # Calling ``stop()`` ensures that there is no residual movement
-    move_group.stop()
+# The go command can be called with joint values, poses, or without any
+# parameters if you have already set the pose or joint target for the group
+move_group.go(joint_goal, wait=True)
 
-
-def go_to_goal(x=0, y=0, z=0, w=0):
-    q = Quaternion(*tf.transformations.quaternion_from_euler(np.pi, 0, 0))
-    pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation = q
-    pose_goal.position.x = x
-    pose_goal.position.y = y
-    pose_goal.position.z = z
-
-    move_group.set_pose_target(pose_goal)
-    rospy.loginfo("====>Moving to:\n{}".format(pose_goal))
-    plan = move_group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    move_group.stop()
-    # It is always good to clear your targets after planning with poses.
-    # Note: there is no equivalent function for clear_joint_value_targets()
-    move_group.clear_pose_targets()
-
-def get_current_pose():
-    print(move_group.get_current_pose())
-
-def main():
-   while not rospy.is_shutdown():
-    #   get_current_pose()
-      go_home_pose()
-      go_to_goal(0.5,0.5)
-      go_to_goal(-0.5,0.5)
+# Calling ``stop()`` ensures that there is no residual movement
+move_group.stop()
 
 
-if __name__ == "__main__":
-    try:
-        main()
-    except rospy.ROSInterruptException:
-       print("Stopping node")
-       exit()
+pose_goal = geometry_msgs.msg.Pose()
+pose_goal.orientation.w = 1.0
+pose_goal.position.x = 0.4
+pose_goal.position.y = 0.1
+pose_goal.position.z = 0.4
+
+move_group.set_pose_target(pose_goal)
+
+
+plan = move_group.go(wait=True)
+# Calling `stop()` ensures that there is no residual movement
+move_group.stop()
+# It is always good to clear your targets after planning with poses.
+# Note: there is no equivalent function for clear_joint_value_targets()
+move_group.clear_pose_targets()
 
