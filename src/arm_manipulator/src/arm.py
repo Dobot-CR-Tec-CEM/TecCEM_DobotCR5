ce72b6650345063e8fb2023928d31fc8441c2f36
Daniela <daniela.aluna@outlook.com>
2023-05-16 20:07:58 -0600

    cambios en world

---
+++ src/arm_manipulator/src/arm.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import rospy
+from dobot_bringup.srv import *
+
+def disable_client():
+    rospy.wait_for_service('/dobot_bringup/srv/DisableRobot')
+    
+    try:
+        disable = rospy.ServiceProxy('/dobot_bringup/srv/DisableRobot', DisableRobot)
+        resp = disable()
+        return resp
+    except rospy.ServiceException as e:
+        print("Service Failed:", e)
+
+
+def enable_client():
+    rospy.wait_for_service('/dobot_bringup/srv/EnableRobot')
+    
+    try:
+        enable = rospy.ServiceProxy('/dobot_bringup/srv/EnableRobot', EnableRobot)
+        resp = enable()
+        return resp
+    except rospy.ServiceException as e:
+        print("Service Failed:", e)
+
+
+def go_to_point(x, y, z, rx, ry, rz):
+    rospy.wait_for_service('/dobot_bringup/srv/ServoP')
+    
+    try:
+        move = rospy.ServiceProxy('/dobot_bringup/srv/ServoP', ServoP)
+        resp = move(x, y, z, rx, ry, rz)
+        return resp
+    except rospy.ServiceException as e:
+        print("Service Failed:", e)
+
+
+def speedFactor():
+    rospy.wait_for_service("/dobot_bringup/srv/SpeedFactor")
+    
+    try:
+        speed = rospy.ServiceProxy("/dobot_bringup/srv/SpeedFactor", SpeedFactor)
+        resp = speed(20)
+        return resp
+    except rospy.ServiceException as e:
+        print("Service Failed:", e)
+
+
+def clearError():
+    rospy.wait_for_service("/dobot_bringup/srv/ClearError")
+    
+    try:
+        clear = rospy.ServiceProxy("/dobot_bringup/srv/ClearError", ClearError)
+        resp = clear()
+        return resp
+    except rospy.ServiceException as e:
+        print("Service Failed:", e)
+
+
+def moveJoints(j1, j2, j3, j4, j5, j6):
+    rospy.wait_for_service("/dobot_bringup/srv/JointMovJ")
+    
+    try:
+        move = rospy.ServiceProxy("/dobot_bringup/srv/JointMovJ", JointMovJ)
+        resp = move(j1, j2, j3, j4, j5, j6)
+        return resp
+    except rospy.ServiceException as e:
+        print("Service Failed:", e)
+
+def sync():
+    rospy.wait_for_service("/dobot_bringup/srv/Sync")
+    
+    try:
+        sync = rospy.ServiceProxy("/dobot_bringup/srv/Sync", Sync)
+        resp = sync()
+        return resp
+    except rospy.ServiceException as e:
+        print("Service Failed:", e)
+
+
+
+
+if __name__ == "__main__":
+    #sync()
+    # clearError()
+    # enable_client()
+    speedFactor()
+    #print(moveJoints(160, 50, 30, 10, -90, -90))
+    print("Ya se movio")
+    print(moveJoints(0, 0, 0, 0,0,0))
+    print(go_to_point(500, 500, 500, -80, 0, -100))
+    
+    #
+
+    
--- src/arm_manipulator/src/move_arm.py
+++ src/arm_manipulator/src/move_arm.py
@@ -23,10 +23,10 @@ scene = moveit_commander.PlanningSceneInterface()
 group_name = "cr5_arm"
 move_group = moveit_commander.MoveGroupCommander(group_name)
 
+rate = rospy.Rate(100)
 
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                               moveit_msgs.msg.DisplayTrajectory,
-                                               queue_size=20)
+                                               moveit_msgs.msg.DisplayTrajectory)
 
 # We can get the name of the reference frame for this robot:
 planning_frame = move_group.get_planning_frame()
@@ -73,12 +73,30 @@ def go_home_pose():
     joint_goal[5] = 0
     # The go command can be called with joint values, poses, or without any
     # parameters if you have already set the pose or joint target for the group
+    move_group.go(joint_goal, wait=False)
+    # Calling ``stop()`` ensures that there is no residual movement
+    move_group.stop()
+    rospy.sleep(30)
+
+
+def move_joints():
+    # We can get the joint values from the group and adjust some of the values:
+    joint_goal = move_group.get_current_joint_values()
+    joint_goal[0] = 0
+    joint_goal[1] = 0
+    joint_goal[2] = 0
+    joint_goal[3] = 0
+    joint_goal[4] = 0
+    joint_goal[5] = 0
+    # The go command can be called with joint values, poses, or without any
+    # parameters if you have already set the pose or joint target for the group
     move_group.go(joint_goal, wait=True)
     # Calling ``stop()`` ensures that there is no residual movement
     move_group.stop()
 
 
 def go_to_goal(x=0, y=0, z=0, w=0):
+    global robot
     q = Quaternion(*tf.transformations.quaternion_from_euler(np.pi, 0, 0))
     pose_goal = geometry_msgs.msg.Pose()
     pose_goal.orientation = q
@@ -86,30 +104,64 @@ def go_to_goal(x=0, y=0, z=0, w=0):
     pose_goal.position.y = y
     pose_goal.position.z = z
 
-    move_group.set_pose_target(pose_goal)
-    rospy.loginfo("====>Moving to:\n{}".format(pose_goal))
-    plan = move_group.go(wait=True)
+    move_group.set_pose_target(pose_goal, "Link6")
+    plan = move_group.plan()
+
+    display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+    display_trajectory.trajectory_start = robot.get_current_state()
+    display_trajectory.trajectory.append(plan)
+    display_trajectory_publisher.publish(display_trajectory)
+
+    move_group.go(wait=False)
+    #move_group.clear_pose_targets()
+    move_group.set_start_state_to_current_state()
+    nueva_pose = move_group.get_current_state()
+    print("JOINT VALUES", nueva_pose)
+
+    move_group.execute(plan, wait=True)
+    #rospy.loginfo("====>Moving to:\n{}".format(pose_goal))
+    #sucess = move_group.move(wait=True)
+    #print(sucess)
     # Calling `stop()` ensures that there is no residual movement
     move_group.stop()
     # It is always good to clear your targets after planning with poses.
     # Note: there is no equivalent function for clear_joint_value_targets()
-    move_group.clear_pose_targets()
+
+    rospy.sleep(20)
 
 def get_current_pose():
     print(move_group.get_current_pose())
 
 def main():
    while not rospy.is_shutdown():
+      print("HOME")
     #   get_current_pose()
       go_home_pose()
-      go_to_goal(0.5,0.5)
-      go_to_goal(-0.5,0.5)
+      print("PUNTO 1")
+      go_to_goal(0.5,0.5, 0.5)
+      #move_joints()
+      print("PUNTO 2")
+      go_to_goal(-0.5,0.5, 0.5)
+      print("PUNTO 3")
+      go_to_goal(-0.5,-0.5, 0.5)
+    #   rospy.sleep(20)
 
 
 if __name__ == "__main__":
     try:
         main()
+        # print("HOME")
+        # go_home_pose()
+        # print("PUNTO 1")
+        # go_to_goal(0.5,0.5, 0.5)
+        # print("PUNTO 2")
+        # go_to_goal(-0.5,0.5, 0.5)
+        # print("PUNTO 3")
+        # go_to_goal(-0.5,-0.5, 0.5)
+        # print("HOME")
+        # go_home_pose()
     except rospy.ROSInterruptException:
        print("Stopping node")
        exit()
+exit()
 
